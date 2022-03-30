37a627880be7bfffe93e7d0cf2ef81bcdb9383b7
TanyaT <tanyatodorova5@gmail.com>
2022-03-30 15:31:23 +0200

    Added components and created the models

---
+++ admin/src/components/context.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createContext, useContext, useState } from 'react'
+
+
+export const UserContext = createContext()
+export default function UserProvider({children}) {
+    const [userData, setUserData] = useState(null)
+
+    return <UserContext.Provider value = {{userData, setUserData}}>
+        {children}
+    </UserContext.Provider>
+}
