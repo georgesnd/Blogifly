905b5581647a656be6e47f7262dc7f2b03ba1961
TanyaT <tanyatodorova5@gmail.com>
2022-04-04 12:01:43 +0200

    Middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const Admin = require('../models/Admin')
+
+module.exports = (req, res) =>{
+    try {
+     
+        const token = req.cookies.recapcookie
+        console.log('auth here, token is', token);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log('auth : decoded is', decoded);
+
+        const admin = await Admin.findById(decoded.id)
+        if(!admin) return res.send({success:false})
+
+        next()
+
+    } catch (error) {
+        res.send(error.message)
+    }
+}
