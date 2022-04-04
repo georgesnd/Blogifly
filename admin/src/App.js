72eb062b0bed4a5fd698c12e9090aa8d6e119e04
TanyaT <tanyatodorova5@gmail.com>
2022-04-04 11:58:45 +0200

    Implemented routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,14 +2,25 @@ import './App.css'
 import Home from './components/Home'
 
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AddPost from './components/AddPost'
+import Login from './components/Login'
+import Register from './components/Register'
+
 
 function App() {
 
   return (
     <div className='App'>
-        <Home/>
+         <Routes>
+        <Route path='/home' exact element={<Home/>} />
+        {/* <Route path='/profile' exact element={<Profile />}/> */}
+        <Route path='/addpost' exact element={<AddPost />}/>
+        <Route path= '/login' exact element={<Login />} />  
+        <Route path= '/register' exact element={<Register/>} /> 
+    </Routes> 
     </div>
-  )
+  )  
 }
 
 export default App
