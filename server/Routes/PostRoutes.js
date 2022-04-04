ed7758762c596cd6705c7ebe9c8a80a1a80f166f
TanyaT <tanyatodorova5@gmail.com>
2022-04-04 12:02:29 +0200

    Auth for post routes

---
+++ server/Routes/PostRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const postController = require('../Controlers/PostControllers')
+
+const auth = require('../middlewares/auth')
+
+router.post('/create', auth, postController.createPost)
+router.delete('/delete/:postId', auth, postController.deletePost)
+router.patch('/edit/:postId', auth,postController.editPost)
+router.get('/list', auth, postController.listPosts)
+router.get('/get/:postId', auth, postController.getPost)
+router.post('/addComment/:postId', auth, postController.addComment)
+router.delete('/deleteComment/:postId/:commentId', auth, postController.deleteComment)
+router.patch('/editComment/:postId/:commentId', auth, postController.editComment)
+
+
+module.exports = router 
